b00a519d987b8d028e5722b15000300e53446586
sukhmander <sukhpaysolution73073@gmail.com>
2023-02-05 12:42:30 +0530

    local chnges added

--- resources/views/pages/admin/createprofile.blade.php
+++ resources/views/pages/admin/createprofile.blade.php
@@ -22,9 +22,8 @@
 @stop
 
 @section('content') 
-<x-adminlte-select 
-onchange="changeHandler"
-name="role_type" label="Select Role Here *" fgroup-class="col-md-3" enable-old-support>
+<x-adminlte-select onchange="changeHandler()" id="select_id"
+name="role_type" label="Select Role Here *" fgroup-class="tttt col-md-3" enable-old-support>
         @php ($occupations = ['Admin' , 'Teacher' ,'Student'])
         @foreach ($occupations as $occupation)
             <option  value="{{$occupation}}" >{{$occupation}}</option>
@@ -32,11 +31,43 @@ name="role_type" label="Select Role Here *" fgroup-class="col-md-3" enable-old-s
     </x-adminlte-select>
 
   
+    <div id="admin" >
     @livewire('create-admin-form')
+    </div>
+    <div id="teacher" class="d-none">
+    @livewire('create-teacher-form')
+    </div>
+    <div id="student" class="d-none">
+    @livewire('create-student-form')
+    </div>
+    
+
+
     @livewire('display-status')
-    <scrip>
-        function changeHandler(e){
-            console.log("Tests ",e)
-        }
-    </scrip>
+    
+    
+    adminlte
+
+    <script>
+function changeHandler() {
+    d = document.getElementById("select_id").value;
+    if(d=="Admin"){
+        document.getElementById("admin").classList.remove('d-none');
+        document.getElementById("teacher").classList.add('d-none');
+        document.getElementById("student").classList.add('d-none');
+    }else if(d=="Teacher"){
+        document.getElementById("admin").classList.add('d-none');
+        document.getElementById("teacher").classList.remove('d-none');
+        document.getElementById("student").classList.add('d-none');
+    
+    }else if(d=="Student"){
+        document.getElementById("admin").classList.add('d-none');
+        document.getElementById("teacher").classList.add('d-none');
+        document.getElementById("student").classList.remove('d-none');
+    
+    }
+   
+}
+</script>
+
 @stop
