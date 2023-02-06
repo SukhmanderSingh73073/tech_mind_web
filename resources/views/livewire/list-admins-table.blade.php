edf77692fdf87bb17549403c1e91dbbfba141ccd
Ram <ram@peregrine-it.com>
2023-02-06 21:05:46 +0530

    admin action btn

--- resources/views/livewire/list-admins-table.blade.php
+++ resources/views/livewire/list-admins-table.blade.php
@@ -3,7 +3,7 @@
         <h4 class="card-title">Admins </h4>
     </div>
     <div class="card-body">
-        <x-adminlte-datatable id="school-list-table" 
+        <x-adminlte-datatable id="school-list-table"
         :heads="['S/N', 'Name','email','gender' ,
          'address', 'Action' , 'Delete']" class='text-capitalize' bordered striped head-theme="dark" beautify>
             @foreach($admins as $admin)
@@ -13,56 +13,24 @@
                 <td style="text-transform: none">{{ $admin->email}}</td>
                 <td>{{$admin->gender}}</td>
                 <td>{{$admin->address}}</td>
-
-
-                <!-- <td>
-                 <a href="route('admins.edit', $admin->id)">Edit</a>
-                 
-                </td> -->
-             
-                <td>@livewire('dropdown-links', [
+                {{-- check file --}}
+                {{-- {{/resources/views/pages/admin/index.blade.php}} --}}
+                <td>
+                  <button  class="btn btn-primary" onclick="actionHandle('/dashboard/admins/{{$admin->id}}/edit')"   ><i class="fas fa-pen"></i></button>
+                  <button  class="btn btn-secondary" onclick="actionHandle('/dashboard/admins/{{$admin->id}}')" data-url="admins.edit" data-id="{{$admin->id}}"  ><i class="fas fa-eye "></i></button>
+                </td>
+                {{-- <td>@livewire('dropdown-links', [
                     'links' => [
                     ['href' => route("admins.edit", $admin->id), 'text' => 'Edit profile', 'icon' => 'fas fa-cog'],
                     ['href' => route("admins.show", $admin->id), 'text' => 'View', 'icon' => 'fas fa-eye'],
                     ],
                     ],)
-                </td>               
-
-
+                </td> --}}
                 <td>
                     @livewire('delete-modal', ['modal_id' => $admin->id ,"action" => route('admins.destroy', $admin->id), 'item_name' => $admin->name])
-                </td> 
-
-
-
-
-
+                </td>
             </tr>
             @endforeach
         </x-adminlte-datatable>
     </div>
-</div>
-
-
-               <!-- @if (strpos(url()->current(), 'edit_admin') !== false)
-                <td>@livewire('dropdown-links', [
-                    'links' => [
-                    ['href' => route("admins.edit", $admin->id), 'text' => 'Edit profile', 'icon' => 'fas fa-cog'],
-                    ],
-                    ],)</td>
-                @elseif (strpos(url()->current(), 'delete_admin') !== false)
-                <td>
-                    @livewire('delete-modal', ['modal_id' => $admin->id ,"action" => route('admins.destroy', $admin->id), 'item_name' => $admin->name])
-                </td> 
-                @else
-                <td>@livewire('dropdown-links', [
-                    'links' => [
-                    ['href' => route("admins.edit", $admin->id), 'text' => 'Edit profile', 'icon' => 'fas fa-cog'],
-                    ['href' => route("admins.show", $admin->id), 'text' => 'View', 'icon' => 'fas fa-eye'],
-                    ],
-                    ],)
-                </td>
-                
-                @endif  
-
-                
+ </div>
--- resources/views/pages/admin/index.blade.php
+++ resources/views/pages/admin/index.blade.php
@@ -4,12 +4,12 @@
 
 
 @section('content_header')
-    <h1 class=""> 
+    <h1 class="">
         {{ __('Admins') }}
     </h1>
 
     @livewire('show-set-school')
-    
+
     @livewire('breadcrumbs', ['paths' => [
         ['href'=> route('dashboard'), 'text'=> 'Dashboard'],
         ['href'=> route('admins.index'), 'text'=> 'admins', 'active'],
@@ -17,21 +17,26 @@
 
 @stop
 
-@section('content') 
+@section('content')
 @livewire('school-set')
 <div style='text-align:right' class='col-12'>
     <row>
-    
+
     <form action={{ route("admins.create") }}>
     <x-adminlte-button type="submit" label="Create New Admin"  theme="primary" icon="fas fa-key"  />
     <!-- <x-adminlte-button label="Bulk Upload" theme="primary" icon="fas fa-key"  /> -->
     </form>
-    
+
     </row>
-                
+
       </div>
-      
+
 
     @livewire('list-admins-table')
     @livewire('display-status')
+    <script>
+        function actionHandle(url){
+            window.open(window.location.origin+url,"_self")
+        }
+    </script>
 @stop
